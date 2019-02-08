6e568e5a1233e0ee1dbd95090bbc1d0e0697187c
Rik Lomas <rik@superhi.com>
2019-02-08 17:40:02 -0500

    added import to admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -12,4 +14,36 @@ permit_params :first_name, :last_name, :email, :password, :password_confirmation
 #   permitted
 # end
 
+  action_item :import_csv, only: :index do
+    link_to 'Import CSV', import_csv_admin_users_path, method: :get
+  end
+
+  collection_action :import_csv do
+    render "admin/users/import_csv"
+  end
+
+  collection_action :importing_csv, method: :post do
+    csv_data = params[:csv][:csv]
+
+    headers = true
+    CSV.parse(csv_data.read) do |row|
+      if headers
+        headers = false
+      else
+        first_name = row[0]
+        last_name = row[1]
+        email = row[2]
+        insta = row[3]
+        points = row[4]
+        discount = row[5]
+
+        # @user = User.find_by(email: email)
+        # if @user.present?
+        #   @user.update_attributes(points: points)
+        # end
+      end
+    end
+
+    redirect_to collection_path, notice: "CSV imported successfully!"
+  end
 end
