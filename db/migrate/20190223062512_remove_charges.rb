420f192b36eea757536d7c03080a3e9a81680ab8
Rik Lomas <rik@superhi.com>
2019-04-24 21:36:46 -0400

    no charges table

--- db/migrate/20190223062512_remove_charges.rb
+++ db/migrate/20190223062512_remove_charges.rb
@@ -1,5 +1,5 @@
 class RemoveCharges < ActiveRecord::Migration[5.2]
   def change
-    drop_table :charges
+    drop_table :charges, if_exists: true
   end
 end
--- db/migrate/20190223070103_drop_tables.rb
+++ db/migrate/20190223070103_drop_tables.rb
@@ -1,7 +1,7 @@
 class DropTables < ActiveRecord::Migration[5.2]
   def change
 
-  drop_table :charges
-  drop_table :coupons
+  drop_table :charges, if_exists: true
+  drop_table :coupons, if_exists: true
   end
 end
