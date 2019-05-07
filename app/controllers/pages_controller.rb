973a73adc39376f882f4f7eb7a6ba937276131b7
Anita Cheung <hello@anitacheung.ca>
2019-05-06 23:32:48 -0700

    changed CSS of Bettyland page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def bettyland
     @user= @current_user
+    @users= User.order("created_at DESC").limit(3)
     @newsletters= Newsletter.order("created_at desc").limit(3)
   end
 
