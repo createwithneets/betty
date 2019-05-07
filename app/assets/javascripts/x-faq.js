d8fa2a6d159820dd319eaabe03fbf4c68e970f6d
Anita Cheung <hello@anitacheung.ca>
2019-05-07 13:16:02 -0700

    edited JS for FAQ

--- app/assets/javascripts/x-faq.js
+++ app/assets/javascripts/x-faq.js
@@ -1,9 +1,49 @@
 /*open faq*/
 
-const registerButton = document.querySelector('.register-button')
+const registerButton = document.querySelector('.js-rb-1')
 registerButton.addEventListener('click', event => {
   // stops any default actions from happening
   event.preventDefault()
-  const frontEl = document.querySelector('.front')
+  const frontEl = document.querySelector('.js-front-1')
+  frontEl.classList.add('fade')
+})
+
+const registerButton = document.querySelector('.js-rb-2')
+registerButton.addEventListener('click', event => {
+  // stops any default actions from happening
+  event.preventDefault()
+  const frontEl = document.querySelector('.js-front-2')
+  frontEl.classList.add('fade')
+})
+
+const registerButton = document.querySelector('.js-rb-3')
+registerButton.addEventListener('click', event => {
+  // stops any default actions from happening
+  event.preventDefault()
+  const frontEl = document.querySelector('.js-front-3')
+  frontEl.classList.add('fade')
+})
+
+const registerButton = document.querySelector('.js-rb-4')
+registerButton.addEventListener('click', event => {
+  // stops any default actions from happening
+  event.preventDefault()
+  const frontEl = document.querySelector('.js-front-4')
+  frontEl.classList.add('fade')
+})
+
+const registerButton = document.querySelector('.js-rb-5')
+registerButton.addEventListener('click', event => {
+  // stops any default actions from happening
+  event.preventDefault()
+  const frontEl = document.querySelector('.js-front-5')
+  frontEl.classList.add('fade')
+})
+
+const registerButton = document.querySelector('.js-rb-6')
+registerButton.addEventListener('click', event => {
+  // stops any default actions from happening
+  event.preventDefault()
+  const frontEl = document.querySelector('.js-front-6')
   frontEl.classList.add('fade')
 })
