fdb0908ea186980cefd8876f58e05288ee2203b1
Anita Cheung <hello@anitacheung.ca>
2019-05-10 06:36:51 -0700

    fixed product index error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,6 @@ class ProductsController < ApplicationController
   def index
     @products= Product.all
     @user= current_user
-    @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
-    @unread_messages = Message.where(conversation: @conversations).where.not(user: current_user).where(read: false).count
   end
 
   def show
